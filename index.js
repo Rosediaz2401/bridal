4553bd3c919d2f936ba566bf26e15e9db286b2ae
rosediaz <rosa.diaz.aguilar@gmail.com>
2022-06-16 18:03:53 -0500

    faq terminado responsive y desktop

--- index.js
+++ index.js
@@ -7,5 +7,12 @@ d.addEventListener("DOMContentLoaded",e=>{
     hamburgerMenu(".panel-btn",".panel", ".menu a");
     shareIcons(".share",".social-wrap",".media-icons")
     discoverMorePhotos("#discover-btn",".show-more","#show-less-btn")
+    // questions(".faq-text")
 });
 
+const test = Array.from(d.getElementsByClassName('faq-text'))
+    test.forEach(item =>{
+        item.addEventListener('click',(e) =>{
+            e.target.parentElement.classList.toggle('my-active')
+        })
+    })
